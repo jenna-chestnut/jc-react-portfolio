8d0ed9d37446c68c9b36086560ef7d0a104e3b99
Jenna Chestnut <jennachestnut@Jennas-MacBook-Pro.local>
2024-01-31 00:14:43 -0500

    fixed broken form

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -2,30 +2,33 @@ import './ContactForm.css';
 import React from 'react';
 
 class ContactForm extends React.Component {
-  render() {
+	render() {
+		return (
+			<div className="wrapper">
+				<form action="https://formspree.io/f/mayrqnpv" method="post">
+					<fieldset>
+						<legend>SEND A MESSAGE</legend>
+						<p>Inquiries | Recruitment | Networking | Feedback</p>
 
-      return (
-        <div className="wrapper">
-        <form action="https://formspree.io/f/mayrqnpvp" method="post">
-          <fieldset>
-            <legend>SEND A MESSAGE</legend>
-            <p>Inquiries | Recruitment | Networking | Feedback</p>
+						<label htmlFor="name">NAME:</label>
+						<input type="text" id="name" name="name" />
 
-            <label htmlFor="name">NAME:</label>
-            <input type="text" id="name" name="name" />
+						<label className="email" htmlFor="emailaddress">
+							EMAIL:
+						</label>
+						<input type="email" name="_replyto" id="emailaddress" />
 
-            <label className="email" htmlFor="emailaddress">EMAIL:</label>
-            <input type="email" name="_replyto" id="emailaddress" />
+						<label className="email" htmlFor="email">
+							MESSAGE:
+						</label>
+						<textarea id="email" name="message"></textarea>
 
-            <label className="email" htmlFor="email">MESSAGE:</label>
-            <textarea id="email" name="message"></textarea>
-
-            <button type="submit">SEND</button>
-          </fieldset>
-        </form>
-      </div>
-      );
-    }
+						<button type="submit">SEND</button>
+					</fieldset>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default ContactForm;
